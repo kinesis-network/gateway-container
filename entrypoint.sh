aea47601fc5d93c2f1b659826e5a2ea6fee43b44
Toshihito Kikuchi <k.toshihito@yahoo.de>
2025-10-05 15:07:22 -0700

    Need to use iptables-legacy if legacy NAT is loaded

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ if [ -f /etc/haproxy/haproxy.cfg ]; then
 fi
 
 if [ -f /etc/wireguard/${IFACE_NAME}.conf ]; then
+  if lsmod | grep -q iptable_nat; then
+    echo "[*] Enabling iptables-legacy as default"
+    update-alternatives --set iptables /usr/sbin/iptables-legacy
+  fi
   echo "[*] Starting WireGuard (${IFACE_NAME})"
   wg-quick up /etc/wireguard/${IFACE_NAME}.conf
 fi
